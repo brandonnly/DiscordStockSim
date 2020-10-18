6332b15660142ad7fdc7ac5febaf6b9cf8d47427
brandonnly <bly852@gmail.com>
2020-10-18 18:25:23 -0400

    working on pull functions

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-import pymongo
 from pymongo import MongoClient
 from settings import *
 
@@ -6,155 +5,69 @@ cluster = MongoClient(DATABASE)
 db = cluster[DB_NAME]
 collection = db[COLLECTION_NAME]
 
-post = {
-    "_id": 0,
-    "cash_balances": {
-        "Server_0": default_balance,
-        "Server_1": default_balance,
-        "Server_2": default_balance,
-    },
-    "portfolios": {
-        "Server_0": {
-            "AAPL": 0,
-            "MSFT": 0,
-            "AMZN": 0,
-            "GOOG": 0,
-            "FB": 0,
-            "TSLA": 0,
-            "TSM": 0,
-            "NVDA": 0,
-            "PFE": 0,
-            "T": 0,
-            "ZM": 0,
-            "AMD": 0,
-            "BA": 0,
-            "SQ": 0,
-            "GE": 0,
-            "ADSK": 0,
-            "VRTX": 0,
-            "PTON": 0,
-            "NIO": 0,
-            "F": 0,
-            "CHWY": 0,
-            "PLTR": 0,
-            "NET": 0,
-            "DKNG": 0,
-            "FSLY": 0,
-            "ARRY": 0,
-            "HYLN": 0,
-            "WKHS": 0,
-        },
-        "Server_1": {
-            "AAPL": 0,
-            "MSFT": 0,
-            "AMZN": 0,
-            "GOOG": 0,
-            "FB": 0,
-            "TSLA": 0,
-            "TSM": 0,
-            "NVDA": 0,
-            "PFE": 0,
-            "T": 0,
-            "ZM": 0,
-            "AMD": 0,
-            "BA": 0,
-            "SQ": 0,
-            "GE": 0,
-            "ADSK": 0,
-            "VRTX": 0,
-            "PTON": 0,
-            "NIO": 0,
-            "F": 0,
-            "CHWY": 0,
-            "PLTR": 0,
-            "NET": 0,
-            "DKNG": 0,
-            "FSLY": 0,
-            "ARRY": 0,
-            "HYLN": 0,
-            "WKHS": 0,
-        },
-        "Server_2": {
-            "AAPL": 0,
-            "MSFT": 0,
-            "AMZN": 0,
-            "GOOG": 0,
-            "FB": 0,
-            "TSLA": 0,
-            "TSM": 0,
-            "NVDA": 0,
-            "PFE": 0,
-            "T": 0,
-            "ZM": 0,
-            "AMD": 0,
-            "BA": 0,
-            "SQ": 0,
-            "GE": 0,
-            "ADSK": 0,
-            "VRTX": 0,
-            "PTON": 0,
-            "NIO": 0,
-            "F": 0,
-            "CHWY": 0,
-            "PLTR": 0,
-            "NET": 0,
-            "DKNG": 0,
-            "FSLY": 0,
-            "ARRY": 0,
-            "HYLN": 0,
-            "WKHS": 0,
-        },
-    },
-}
-
-#collection.insert_one(post)
-
 
 def new_user(user_id):
     collection.insert_one({"_id": user_id})
+    print("Added new user with id", user_id)
 
 
-def server_add_user(user_id, server_id):
+def add_user(user_id, server_id):
     cash_balance = "cash_balances." + str(server_id)
     portfolio = "portfolio." + str(server_id)
     collection.update_one({"_id": user_id}, {"$set": {cash_balance: default_balance}})
     collection.update_one({"_id": user_id}, {"$set": {portfolio: {"AAPL": 0,
-                                                       "MSFT": 0,
-                                                       "AMZN": 0,
-                                                       "GOOG": 0,
-                                                       "FB": 0,
-                                                       "TSLA": 0,
-                                                       "TSM": 0,
-                                                       "NVDA": 0,
-                                                       "PFE": 0,
-                                                       "T": 0,
-                                                       "ZM": 0,
-                                                       "AMD": 0,
-                                                       "BA": 0,
-                                                       "SQ": 0,
-                                                       "GE": 0,
-                                                       "ADSK": 0,
-                                                       "VRTX": 0,
-                                                       "PTON": 0,
-                                                       "NIO": 0,
-                                                       "F": 0,
-                                                       "CHWY": 0,
-                                                       "PLTR": 0,
-                                                       "NET": 0,
-                                                       "DKNG": 0,
-                                                       "FSLY": 0,
-                                                       "ARRY": 0,
-                                                       "HYLN": 0,
-                                                       "WKHS": 0, }}})
+                                                                  "MSFT": 0,
+                                                                  "AMZN": 0,
+                                                                  "GOOG": 0,
+                                                                  "FB": 0,
+                                                                  "TSLA": 0,
+                                                                  "TSM": 0,
+                                                                  "NVDA": 0,
+                                                                  "PFE": 0,
+                                                                  "T": 0,
+                                                                  "ZM": 0,
+                                                                  "AMD": 0,
+                                                                  "BA": 0,
+                                                                  "SQ": 0,
+                                                                  "GE": 0,
+                                                                  "ADSK": 0,
+                                                                  "VRTX": 0,
+                                                                  "PTON": 0,
+                                                                  "NIO": 0,
+                                                                  "F": 0,
+                                                                  "CHWY": 0,
+                                                                  "PLTR": 0,
+                                                                  "NET": 0,
+                                                                  "DKNG": 0,
+                                                                  "FSLY": 0,
+                                                                  "ARRY": 0,
+                                                                  "HYLN": 0,
+                                                                  "WKHS": 0, }}})
+    print("Added server", server_id, "portfolio to user", user_id)
 
 
 def set_stock(user_id, server_id, stock, quantity):
     updated_stock = "portfolio." + str(server_id) + "." + stock
-    collection.update_one({"_id": user_id}, {"$set": {updated_stock: quantity} })
+    collection.update_one({"_id": user_id}, {"$set": {updated_stock: quantity}})
+    print("Set the stock of", user_id, "in", server_id, "to", quantity, "for stock", stock)
 
 
-def get_stock(user_id, server_id, stock, quantity):
+def get_stock(user_id, server_id, stock):
+    portfolio = str(user_id) + ".portfolio." + str(server_id)
+    stocks = collection.find_one({"_id": "123.234"})
+    return stocks
 
 
 def set_balance(user_id, server_id, balance):
+    updated_balance = "cash_balances." + str(server_id)
+    collection.update_one({"_id": user_id}, {"$set": {updated_balance: balance}})
+    print("Set the balance of", user_id, "in", server_id, "to", balance)
 
+
+def get_balance(user_id, server_id):
+    balance_to_find = str(user_id) + ".cash_balances." + str(server_id)
+    return collection.find(balance_to_find)
+
+
+print(get_stock(123, 234, "AAPL"))
+# print(get_balance(123, 234))
