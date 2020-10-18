c64faba982188e2d2d862e61c9c8f65a8d030f23
vincentrann <vincentrann@github.com>
2020-10-18 18:58:06 -0400

    finish

--- bot.py
+++ bot.py
@@ -1,21 +1,20 @@
 import discord
 from discord.ext import commands
 from settings import *
+from database import *
+from datascraping import *
+
 
 
 bot = discord.ext.commands.Bot('s!')
 
-
 @bot.command()
-async def hello(ctx):
-    await ctx.send("hi!")
-
-'''@bot.event():
-async def on_member_join(member):
-
-    print(f'(member) joined the server.')
-async def on_member_remove(member):
-    print(f'(member) has left a sever.')'''
+async def join(ctx):
+    user_id = ctx.author.id
+    server_id = ctx.author.id
+    new_user(ctx.author.id)
+    add_user(ctx.author.id, ctx.guild.id)
+    await ctx.send('you are now in stonks kekw')
 
 bot.run(BOT_TOKEN)
 
