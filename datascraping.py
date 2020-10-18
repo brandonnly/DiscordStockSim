507e1b43c364a0e19dc892aef74ba0e882af5a9f
sniper2x5 <58059019+sniper2x5@users.noreply.github.com>
2020-10-18 18:23:12 -0400

    Create datascraping.py

---
+++ datascraping.py
@@ -0,0 +1,9 @@
+import finnhub
+from settings import *
+finnhub_client = finnhub.Client(api_key = FINHUB_API_KEY)
+
+def price (stock):
+    numbers = finnhub_client.quote(stock)
+    return numbers["c"]
+
+print (price(("AAPL"))) 
