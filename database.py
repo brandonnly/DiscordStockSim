1c5b083144428708ad23ca2362a4322a142e5551
brandonnly <bly852@gmail.com>
2020-10-18 15:49:11 -0400

    created new_user and server_add_user
    new_user function to create a new user in the database
    server_add_user to create a new portfolio and default balance for the user

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ import pymongo
 from pymongo import MongoClient
 from settings import *
 
-
 cluster = MongoClient(DATABASE)
 db = cluster[DB_NAME]
 collection = db[COLLECTION_NAME]
@@ -10,9 +9,9 @@ collection = db[COLLECTION_NAME]
 post = {
     "_id": 0,
     "cash_balances": {
-        "Server_0": 10000,
-        "Server_1": 10000,
-        "Server_2": 10000,
+        "Server_0": default_balance,
+        "Server_1": default_balance,
+        "Server_2": default_balance,
     },
     "portfolios": {
         "Server_0": {
@@ -105,9 +104,45 @@ post = {
             "HYLN": 0,
             "WKHS": 0,
         },
-
-
     },
 }
 
-collection.insert_one(post)
+#collection.insert_one(post)
+
+
+def new_user(user_id):
+    collection.insert_one({"_id": user_id})
+
+
+def server_add_user(user_id, server_id):
+    cash_balance = "cash_balances." + str(server_id)
+    portfolio = "portfolio." + str(server_id)
+    collection.update_one({"_id": user_id}, {"$set": {cash_balance: default_balance}})
+    collection.update_one({"_id": user_id}, {"$set": {portfolio: {"AAPL": 0,
+                                                       "MSFT": 0,
+                                                       "AMZN": 0,
+                                                       "GOOG": 0,
+                                                       "FB": 0,
+                                                       "TSLA": 0,
+                                                       "TSM": 0,
+                                                       "NVDA": 0,
+                                                       "PFE": 0,
+                                                       "T": 0,
+                                                       "ZM": 0,
+                                                       "AMD": 0,
+                                                       "BA": 0,
+                                                       "SQ": 0,
+                                                       "GE": 0,
+                                                       "ADSK": 0,
+                                                       "VRTX": 0,
+                                                       "PTON": 0,
+                                                       "NIO": 0,
+                                                       "F": 0,
+                                                       "CHWY": 0,
+                                                       "PLTR": 0,
+                                                       "NET": 0,
+                                                       "DKNG": 0,
+                                                       "FSLY": 0,
+                                                       "ARRY": 0,
+                                                       "HYLN": 0,
+                                                       "WKHS": 0, }}})
