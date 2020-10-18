0e65c12dd2a7a55ac0c0eb55fa47a9a41b55618c
vincentrann <vincentrann@github.com>
2020-10-18 17:41:07 -0400

    creat procfile

--- bot.py
+++ bot.py
@@ -10,5 +10,12 @@ bot = discord.ext.commands.Bot('s!')
 async def hello(ctx):
     await ctx.send("hi!")
 
+'''@bot.event():
+async def on_member_join(member):
+
+    print(f'(member) joined the server.')
+async def on_member_remove(member):
+    print(f'(member) has left a sever.')'''
 
 bot.run(BOT_TOKEN)
+
